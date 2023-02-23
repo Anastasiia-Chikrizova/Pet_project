9c11e280cc8640f908a7f19c631e4258de582ac0
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2023-02-23 19:15:31 +0100

    add router config, create navbar, add svg loader , create sidebar

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -4,6 +4,12 @@ import {BuildOptions} from "./types/config";
 
 export function buildLoaders({isDev} : BuildOptions) : webpack.RuleSetRule[] {
 
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+    }
+
+
     const cssLoader = {
         test: /\.s[ac]ss$/i,
         use: [
@@ -24,6 +30,16 @@ export function buildLoaders({isDev} : BuildOptions) : webpack.RuleSetRule[] {
         ]
     }
 
+    const fileLoader = {
+            test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+            use: [
+                {
+                    loader: 'file-loader',
+                },
+            ],
+        }
+
+
     const typeScriptLoader =  {
         test: /\.tsx?$/,
         use: 'ts-loader',
@@ -31,6 +47,8 @@ export function buildLoaders({isDev} : BuildOptions) : webpack.RuleSetRule[] {
     }
 return  [
     typeScriptLoader,
-    cssLoader
+    cssLoader,
+    fileLoader,
+    svgLoader
 ]
 }
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,29 +1,21 @@
 import './styles/index.scss';
-import {Link, Route, Routes} from "react-router-dom";
-import {AboutPageAsync} from "pages/AboutPage/ui/AboutPage.async";
-import {MainPageAsync} from "pages/MainPage/ui/MainPage.async";
-import {Suspense} from "react";
 import {classNames} from "shared/lib/classNames/className";
 import {useTheme} from "app/providers/ThemeProvider";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
+import {Sidebar} from "widgets/Sidebar";
 
 
 const App = () => {
-    const {theme, toggleTheme} = useTheme();
-
+    const {theme} = useTheme();
 
     return (
         <div className={classNames('app', {}, [theme])} >
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная страницв</Link>
-            <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-            <Routes>
-                <Route path={'/about'} element={<AboutPage/>}/>
-                <Route path={'/'} element={<MainPage/>}/>
-            </Routes>
-            </Suspense>
+            <Navbar/>
+            <div className="content-page">
+                <Sidebar/>
+                <AppRouter/>
+            </div>
         </div>
     )
 }
