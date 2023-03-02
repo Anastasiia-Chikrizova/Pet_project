6857025d0985eb339341c481f39c5420e527db81
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2023-03-02 20:08:28 +0100

    fixed language issues, settings babel, add loader and not found page

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -2,10 +2,30 @@ import webpack from 'webpack';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import { BuildOptions } from './types/config';
 
-export function buildLoaders({ isDev } : BuildOptions) : webpack.RuleSetRule[] {
+export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     const svgLoader = {
         test: /\.svg$/,
         use: ['@svgr/webpack'],
+    };
+
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        'i18next-extract',
+                        {
+                            locales: ['ru', 'en'],
+                            keyAsDefaultValue: true,
+                        },
+                    ],
+                ],
+            },
+        },
     };
 
     const cssLoader = {
@@ -16,16 +36,24 @@ export function buildLoaders({ isDev } : BuildOptions) : webpack.RuleSetRule[] {
                 loader: 'css-loader',
                 options: {
                     modules: {
-                        auto: (resPath: string) => Boolean(resPath.includes('.module')),
+                        auto:
+                        // eslint-disable-next-line max-len
+                            (resPath: string) => Boolean(resPath.includes('.module.')),
                         localIdentName: isDev
                             ? '[path][name]__[local]--[hash:base64:5]'
                             : '[hash:base64:8]',
                     },
-
                 },
             },
             'sass-loader',
         ],
+    };
+
+    // Если не используем тайпскрипт - нужен babel-loader
+    const typescriptLoader = {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
     };
 
     const fileLoader = {
@@ -37,15 +65,11 @@ export function buildLoaders({ isDev } : BuildOptions) : webpack.RuleSetRule[] {
         ],
     };
 
-    const typeScriptLoader = {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-    };
     return [
-        typeScriptLoader,
-        cssLoader,
         fileLoader,
         svgLoader,
+        babelLoader,
+        typescriptLoader,
+        cssLoader,
     ];
 }
