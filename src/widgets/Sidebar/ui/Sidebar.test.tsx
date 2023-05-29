ec46621cd850814761ad812eeba47bf7390d937c
Anastasiia-Chikrizova <anastasiia.chikrizova@gmail.com>
2023-05-29 18:40:23 +0200

    fixed npm issues at github actions

--- src/widgets/Sidebar/ui/Sidebar.test.tsx
+++ src/widgets/Sidebar/ui/Sidebar.test.tsx
@@ -1,15 +1,15 @@
-import {fireEvent, screen} from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 
-import {Sidebar} from 'widgets/Sidebar';
-import {componentRender} from "shared/lib/test/componentRender/componentRender";
+import { Sidebar } from 'widgets/Sidebar';
+import { componentRender } from 'shared/lib/test/componentRender/componentRender';
 
 describe('Sidebar', () => {
     test('Sidebar is present', () => {
-        componentRender(<Sidebar/>);
+        componentRender(<Sidebar />);
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
     });
     test('Button is present', () => {
-        componentRender(<Sidebar/>);
+        componentRender(<Sidebar />);
         const toggleBtn = screen.getByTestId('sidebar-toggle');
         expect(screen.getByTestId('sidebar')).toBeInTheDocument();
         fireEvent.click(toggleBtn);
